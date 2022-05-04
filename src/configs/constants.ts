97f0c265b347ac250b785ac72735b34122bc573a
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-05-04 14:01:42 -0300

    primeiro commit

---
+++ src/configs/constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: "mongodb+srv://admin:admin@cluster0.jsu7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    
+}
